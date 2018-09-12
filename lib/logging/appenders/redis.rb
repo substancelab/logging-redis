59ae3459cdb6451e9b7a3648cbfb7864f86514a6
Jakob Skjerning <jakob@mentalized.net>
2018-09-12 14:22:45 +0200

    Basic, working appender
    Very barebones for now as it has absolutely no configuration options. If the
    Redis server cannot be accessed through a `Redis.new`, this won't work.

---
+++ lib/logging/appenders/redis.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+module Logging
+  module Appenders
+    # Accessor / Factory for the Redis appender.
+    def self.redis(*args)
+      return ::Logging::Appenders::Redis if args.empty?
+      ::Logging::Appenders::Redis.new(*args)
+    end
+
+    # Provides an appender that can append log messages to a Redis list
+    #
+    class Redis < ::Logging::Appender
+      include Buffering
+
+      attr_reader :redis
+
+      def initialize(name, opts = {})
+        super(name, opts)
+        configure_buffering(opts)
+        @redis = ::Redis.new
+      end
+
+      # Close the Redis appender.
+      #
+      def close(*_args)
+        super(false)
+      end
+
+      private
+
+      # This method is called by the buffering code when messages need to be
+      # sent out as an Redis.
+      #
+      def canonical_write(str)
+        redis.rpush(name, str)
+      rescue StandardError => err
+        handle_internal_error(err)
+      end
+
+      def handle_internal_error(err)
+        return err if off?
+        self.level = :off
+        ::Logging.log_internal { "appender #{name.inspect} has been disabled" }
+        ::Logging.log_internal_error(err)
+      end
+    end
+  end
+end
---
+++ lib/logging/plugins/redis.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module Logging
+  module Plugins
+    module Redis
+      module_function
+
+      VERSION = "0.0.1"
+
+      # This method will be called by the Logging framework when it first
+      # initializes. Here we require the redis appender code.
+      def initialize_redis
+        require File.expand_path("../appenders/redis", __dir__)
+      end
+    end
+  end
+end
